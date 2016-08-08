2406e4c12612fc03f8d9c7d23e4dc468ff5c0a7b
Seiya Nuta <nuta@seiya.me>
2016-08-08 10:26:42 +0900

    add device model

---
+++ db/migrate/20160808012240_create_devices.rb
@@ -0,0 +1,10 @@
+class CreateDevices < ActiveRecord::Migration[5.0]
+  def change
+    create_table :devices do |t|
+      t.string :name
+      t.references :app, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
