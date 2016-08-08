799dc77547d1eda66bed47f564f9b8c0b8e69901
Seiya Nuta <nuta@seiya.me>
2016-08-08 11:12:52 +0900

    fix devices#status

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -3,12 +3,13 @@ class DevicesController < ApplicationController
   def status
     device = Device.find_or_create_by(name: device_params[:name])
     device.status = device_params[:status]
-    device.save
+    device.save!
+    head :ok
   end
 
   private
 
   def device_params
-    params.require(:device).permit(:name, :status)
+    params.permit(:name, :status)
   end
 end
