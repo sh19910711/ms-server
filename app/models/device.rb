2e43e33e6083962231bef3aa91acd4ee857c8f75
Seiya Nuta <nuta@seiya.me>
2016-08-08 11:15:03 +0900

    optionalize app_id of a device optional

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,5 @@
 class Device < ApplicationRecord
-  belongs_to :app
+  belongs_to :app, optional: true
 
   # ready: The device has booted and is ready for running an app.
   # running: The device is running an app.
