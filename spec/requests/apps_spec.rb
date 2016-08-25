685b1a1edd93bf21767f59d5c4a3f725365fc2ee
Seiya Nuta <nuta@seiya.me>
2016-08-25 21:10:03 +0900

    rescue from ActiveRecord::RecordInvalid

--- spec/requests/apps_spec.rb
+++ spec/requests/apps_spec.rb
@@ -2,11 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Apps", type: :request do
   describe "POST /apps" do
-    it "creates a new app" do
-      name = 'led-blink'
-      post '/apps', params: { name: name }
-      expect(response).to have_http_status(:ok)
-      expect(App.find_by_name(name)).to be_present
+    context "normal" do
+      it "creates a new app" do
+        name = 'led-blink'
+        post '/apps', params: { name: name }
+        expect(response).to have_http_status(:ok)
+        expect(App.find_by_name(name)).to be_present
+      end
+    end
+
+    context "invalid name" do
+      it "fails to create a new app" do
+        name = '0000'
+        post '/apps', params: { name: name }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(App.find_by_name(name)).not_to be_present
+      end
     end
   end
 
