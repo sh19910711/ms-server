4e097976b080f006126df73dd683fec05dc8cb3d
Seiya Nuta <nuta@seiya.me>
2016-09-17 15:28:17 +0900

    add a TODO

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,6 @@
 SUPPORTED_BOARDS = %w(esp8266)
 
+# TODO: down: The device is down.
 # ready: The device has booted and is ready for running an app.
 # running: The device is running an app.
 DEVICE_STATUSES = %w(ready running)
