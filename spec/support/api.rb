ab7960ce51e63571ea181e45910d7718b7de92cc
Seiya Nuta <nuta@seiya.me>
2016-09-17 15:23:17 +0900

    parse json if Content-Type includes "json"

--- spec/support/api.rb
+++ spec/support/api.rb
@@ -1,3 +1,7 @@
 def api(method, path, data = {})
-  send method.downcase, path, params: data
+  send(method.downcase, path, params: data)
+
+  if response.header['Content-Type'].include?('json')
+    return JSON.parse(response.body)
+  end
 end
