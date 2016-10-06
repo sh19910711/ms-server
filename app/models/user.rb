25d09275fbab8cc6c82bb0c4408f79b2c237f0d9
Seiya Nuta <nuta@seiya.me>
2016-10-06 11:46:32 +0900

    require name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+  validates :name, presence: true, uniqueness: true
 end
