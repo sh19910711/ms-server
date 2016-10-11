8ab036d156a38eb4f082d14c522718433bd41ffe
Seiya Nuta <nuta@seiya.me>
2016-10-11 19:03:05 +0900

    add BUILD_STATUSES

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,8 @@
 API_VERSION = '1'
 SUPPORTED_BOARDS = %w(esp8266)
 
+BUILD_STATUSES = %w(queued building success failure)
+
 # TODO: down: The device is down.
 # new: The device is created.
 # ready: The device has booted and is ready for running an app.
