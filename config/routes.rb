86c987442d26f7f020e7ef6b3977d1d0f6eee8ad
Seiya Nuta <nuta@seiya.me>
2016-10-31 09:44:52 +0900

    implement DELETE a device

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,13 @@ Rails.application.routes.draw do
     post '/:team/apps/:app_name/deployments', to: 'apps#deploy'
 
     # Devices API
-    get    '/:team/devices', to: 'devices#index'
-    post   '/:team/devices', to: 'devices#create'
-    put    '/:team/devices/:device_name', to: 'devices#update'
-    put    '/:team/devices/:device_name/relaunch', to: 'devices#relaunch'
-    get    '/:team/devices/:device_name/log', to: 'devices#log'
-    get    '/:team/devices/:device_name/envvars', to: 'envvars#index'
+    get    '/:team/devices',                            to: 'devices#index'
+    post   '/:team/devices',                            to: 'devices#create'
+    put    '/:team/devices/:device_name',               to: 'devices#update'
+    delete '/:team/devices/:device_name',               to: 'devices#destroy'
+    put    '/:team/devices/:device_name/relaunch',      to: 'devices#relaunch'
+    get    '/:team/devices/:device_name/log',           to: 'devices#log'
+    get    '/:team/devices/:device_name/envvars',       to: 'envvars#index'
     put    '/:team/devices/:device_name/envvars/:name', to: 'envvars#update'
     delete '/:team/devices/:device_name/envvars/:name', to: 'envvars#destroy'
 
