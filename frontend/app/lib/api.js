c010213c0354b49e1c03869b04ada1fed22b5f8b
Hiroyuki Sano <sh19910711@gmail.com>
2016-11-06 17:24:24 +0900

    front: use whatwg-fetch instead of superagent

--- frontend/app/lib/api.js
+++ frontend/app/lib/api.js
@@ -1,20 +1,38 @@
-import request from 'superagent';
+import 'whatwg-fetch';
 
-export default class API {
+const TOKEN_KEY = 'api-token';
+
+function handleContent(res) {
+  const contentType = res.headers.get('Content-Type');
+  if (contentType && contentType.indexOf('application/json') != -1) {
+    return res.json();
+  } else {
+    return res;
+  }
+}
+
+class API {
   constructor(token) {
     this.token = token;
   }
 
   signup(params) {
-    return this.send('post', '/api/auth', params);
+    return this.send('post', '/api/auth', params).then(res => {
+      localStorage.setItem(TOKEN_KEY, JSON.stringify(this.token = res.response.headers.get('token')));
+    });
   }
 
   signin(params) {
-    return this.send('post', '/api/auth/sign_in', params);
+    return this.send('post', '/api/auth/sign_in', params).then(res => {
+      localStorage.setItem(TOKEN_KEY, JSON.stringify(this.token = res.response.headers.get('token')));
+    });
   }
 
   signout() {
-    return this.send('delete', `/api/auth/sign_out`);
+    return this.send('delete', '/api/auth/sign_out').then(res => {
+      localStorage.removeItem(TOKEN_KEY);
+      this.token = null;
+    });
   }
 
   apps(team) {
@@ -23,10 +41,22 @@ export default class API {
 
   send(method, url, params) {
     return new Promise((resolve, reject) => {
-      const req = request[method](url)
-      if (this.token) req.set('authorization', `token ${this.token}`);
-      if (params) req.send(params);
-      req.end((err, res) => (err ? reject : resolve)(res));
+      const body = JSON.stringify(params || {});
+      const headers = {'Content-Type': 'application/json'};
+      if (this.token) headers['Authorization'] = `token ${this.token}`;
+      let response;
+      fetch(url, {method, headers, body})
+        .then(res => response = res)
+        .then(handleContent)
+        .then(content => {
+          if (response.ok) {
+            resolve({content, response});
+          } else {
+            reject({content, response});
+          }
+        });
     });
   }
 };
+
+export default new API(JSON.parse(localStorage.getItem(TOKEN_KEY)));
