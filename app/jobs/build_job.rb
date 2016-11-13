867906cd3015744ac0d2c7659d62811e925e368c
Seiya Nuta <nuta@seiya.me>
2016-11-13 11:52:12 +0900

    build_job: set build status after its deployment

--- app/jobs/build_job.rb
+++ app/jobs/build_job.rb
@@ -14,6 +14,8 @@ class BuildJob < ApplicationJob
     tmp_base_dir = RUBY_PLATFORM.include?("darwin") ? "#{Dir.home}/.cs-build-tmp" : Dir.tmpdir
     FileUtils.mkdir_p(tmp_base_dir)
 
+    stdout = ''
+    success = false
     Dir.mktmpdir("cs-build-", tmp_base_dir) do |tmpdir|
       app_zip = File.join(tmpdir, 'app.zip')
       IO.binwrite(app_zip, build.source_file)
@@ -32,32 +34,42 @@ class BuildJob < ApplicationJob
       else
         stdout  = %x[docker run --rm -v #{tmpdir}:/app -t makestack/deviceos 2>&1]
         success = $?.success?
-      end
 
-      logger.info "build ##{build_id}: #{success ? 'success' : 'failure'}"
+        unless success
+          stdout += "\nfailed to build\n"
+          success = false
+          break
+        end
 
-      # update the build status
-      build.status = success ? 'success' : 'failure'
-      build.log = stdout
-      build.source_file = nil
-      build.save!
+        # deploy images
+        group_id = SecureRandom.uuid
+        image_files = Dir[File.join(tmpdir, "*.*.image")]
 
-      unless success
-        break
-      end
+        if image_files == []
+          stdout += "\nno image files to deploy\n"
+          success = false
+          break
+        end
 
-      # deploy images
-      group_id = SecureRandom.uuid
-      Dir[File.join(tmpdir, "*.*.image")].each do |file|
-        logger.info "build ##{build_id}: deploying #{file}"
-        Deployment.create! do |d|
-          d.app      = build.app
-          d.group_id = group_id
-          d.board    = ImageFile.get_board_from_filename(file)
-          d.tag      = build.tag
-          d.image    = File.open(file, 'rb').read
+        image_files.each do |file|
+          logger.info "build ##{build_id}: deploying #{file}"
+          Deployment.create! do |d|
+            d.app      = build.app
+            d.group_id = group_id
+            d.board    = ImageFile.get_board_from_filename(file)
+            d.tag      = build.tag
+            d.image    = File.open(file, 'rb').read
+          end
         end
       end
     end
+
+    logger.info "build ##{build_id}: #{success ? 'success' : 'failure'}"
+
+    # update the build status
+    build.status = success ? 'success' : 'failure'
+    build.log = stdout
+    build.source_file = nil
+    build.save!
   end
 end
