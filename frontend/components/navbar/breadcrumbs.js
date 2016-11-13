e675b1e9440d25cf4e4ba42e6ab8683ebac2143b
Hiroyuki Sano <sh19910711@gmail.com>
2016-11-13 20:14:27 +0900

    front: fix breadcrumbs

--- frontend/components/navbar/breadcrumbs.js
+++ frontend/components/navbar/breadcrumbs.js
@@ -5,9 +5,10 @@ export default {
   props: ['teams', 'path'],
   data() {
     let crumbLink = '';
+    const path = this.$route && this.$route.path || '';
     return {
       team: api.user,
-      crumbs: this.$route.path.substr(1).split('/').map(crumb => {
+      crumbs: path.substr(1).split('/').map(crumb => {
         crumbLink += `/${crumb}`;
         return {
           name: crumb,
