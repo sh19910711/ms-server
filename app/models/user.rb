48c1874786bcbb1ce321421a7194542463ecd48a
Seiya Nuta <nuta@seiya.me>
2016-12-03 21:06:56 +0900

    User: add format validation of name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
+
   include DeviseTokenAuth::Concerns::User
+
+  USER_NAME_REGEX = /\A[a-zA-Z][a-zA-Z0-9\-\_\.]*\z/
   validates :name, presence: true, uniqueness: true,
-            exclusion: { in: RESERVED_USER_NAMES, message: "%{value} is not available." }
+            exclusion: { in: RESERVED_USER_NAMES, message: "%{value} is not available." }, format: { with: USER_NAME_REGEX }
 
   validates :email, presence: true
   has_many :apps, dependent: :destroy
