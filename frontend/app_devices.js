db551408e2b15e04c6889cfff1a87a3aa9808ef5
Seiya Nuta <nuta@seiya.me>
2016-12-13 08:57:21 +0900

    remove frontend

--- frontend/app_devices.js
+++
@@ -1,7 +0,0 @@
-export default {
-  template: require('./app_devices.html'),
-  components: {
-    navbar: require('components/navbar').default,
-    appmenu: require('components/appmenu').default
-  }
-};
