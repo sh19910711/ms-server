f4b8a0b9f3289cee78a74a75a9e3b1716f5cbc4a
Seiya Nuta <nuta@seiya.me>
2016-12-13 17:44:40 +0900

    ui: implement navbar

---
+++ ui/components/navbar.js
@@ -0,0 +1,23 @@
+import api from "api";
+
+const md5 = require("md5");
+
+require("./navbar.scss");
+
+
+export default {
+  name: "nav-bar",
+  template: require("./navbar.html"),
+  data: () => {
+    return {
+      "breadcrumbs": [
+        { title: "home", url: "/" }
+      ]
+    }
+  },
+  computed: {
+    gravatar_url: function() {
+      return "https://www.gravatar.com/avatar/" + md5(api.user.email) + "?s=40&d=mm";
+    }
+  }
+}
