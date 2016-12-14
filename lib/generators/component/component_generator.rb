babdeed6d7c29df747a4eba906ef69c48c8ae712
Seiya Nuta <nuta@seiya.me>
2016-12-14 11:11:37 +0900

    add component generator

---
+++ lib/generators/component/component_generator.rb
@@ -0,0 +1,8 @@
+class ComponentGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+  def copy_initializer_file
+    template "template.html", "ui/components/#{file_name}.html"
+    template "template.scss", "ui/components/#{file_name}.scss"
+    template "template.js",   "ui/components/#{file_name}.js"
+  end
+end
