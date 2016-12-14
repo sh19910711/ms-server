2487dacd9bb0ae7f10d9e3a563f07803af875bc7
Seiya Nuta <nuta@seiya.me>
2016-12-14 22:35:11 +0900

    add some reserved usernames

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,4 +3,47 @@ SUPPORTED_BOARDS = %w(esp8266)
 BUILD_STATUSES = %w(queued building success failure)
 DEVICE_STATUSES = %w(new ready running down relaunch)
 LOGGING_MAX_LINES = 128
-RESERVED_USER_NAMES = %w(auth blog documentation support easter-egg admin)
+RESERVED_USER_NAMES = %w(
+  home
+  apps
+  devices
+  settings
+  builds
+  deployments
+  create
+  me
+  account
+  admin
+  api
+  blog
+  cache
+  changelog
+  enterprise
+  help
+  jobs
+  lists
+  login
+  logout
+  news
+  plans
+  popular
+  projects
+  security
+  search
+  register
+  invite
+  shop
+  jobs
+  translations
+  signup
+  status
+  tour
+  wiki
+  better
+  hosting
+  auth
+  documentation
+  support
+  easter-egg
+  admin
+)
