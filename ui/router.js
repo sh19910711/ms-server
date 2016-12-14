538cc7ae05c9d4cb570aceeca21a3d881e9cd051
Seiya Nuta <nuta@seiya.me>
2016-12-14 16:14:58 +0900

    ui: add devices view

--- ui/router.js
+++ ui/router.js
@@ -4,14 +4,17 @@ import LoginView from "./views/login";
 import AppsView from "./views/apps";
 import AppView from "./views/app";
 import HomeView from "./views/home";
+import DevicesView from "./views/devices";
 
 
 export default new VueRouter({
   mode: "history",
   routes: [
-    { name: "home",  path: "/",               component: Vue.extend(HomeView)  },
-    { name: "apps",  path: "/apps",           component: Vue.extend(AppsView)  },
-    { name: "app",   path: "/apps/:app_name", component: Vue.extend(AppView)   },
-    { name: "login", path: "/login",          component: Vue.extend(LoginView) },
+    { name: "home",    path: "/",               component: Vue.extend(HomeView)  },
+    { name: "apps",    path: "/apps",           component: Vue.extend(AppsView)  },
+    { name: "app",     path: "/apps/:app_name", component: Vue.extend(AppView)   },
+    { name: "devices", path: "/devices",        component: Vue.extend(DevicesView)  },
+    { name: "team_devices", path: "/:team/devices",  component: Vue.extend(DevicesView)  },
+    { name: "login",   path: "/login",          component: Vue.extend(LoginView) },
   ]
 });
