7ec58f076707ac73d3cd3a66fbddb5aefe7e171e
Seiya Nuta <nuta@seiya.me>
2016-12-15 13:09:34 +0900

    ui/navbar: use dropdown component

--- ui/components/navbar.js
+++ ui/components/navbar.js
@@ -1,4 +1,5 @@
 import api from "api";
+import Dropdown from "components/dropdown";
 
 const md5 = require("md5");
 
@@ -7,8 +8,16 @@ require("./navbar.scss");
 
 export default {
   name: "nav-bar",
+  components: {
+    "dropdown": Dropdown
+  },
   template: require("./navbar.html"),
   props: ["breadcrumbs"],
+  data() {
+    return {
+      show_menu: false
+    }
+  },
   computed: {
     gravatar_url: function() {
       return "https://www.gravatar.com/avatar/" + md5(api.user.email) + "?s=40&d=mm";
