fdf293719f9112a7b6c63ba891f3e23edc9f3153
Seiya Nuta <nuta@seiya.me>
2016-12-15 20:05:45 +0900

    ui: implement statusbar

--- ui/components/navbar.js
+++ ui/components/navbar.js
@@ -1,5 +1,6 @@
 import api from "api";
 import Dropdown from "components/dropdown";
+import {get_statusbar, StatusBar} from "components/statusbar";
 
 const md5 = require("md5");
 
@@ -9,18 +10,28 @@ require("./navbar.scss");
 export default {
   name: "navbar",
   components: {
-    "dropdown": Dropdown
+    "dropdown": Dropdown,
+    "statusbar": StatusBar
   },
   template: require("./navbar.html"),
   props: ["breadcrumbs"],
   data() {
     return {
-      show_menu: false
+      show_menu: false,
+      statusbar_status: "",
+      statusbar_body: ""
     }
   },
   computed: {
     gravatar_url: function() {
       return "https://www.gravatar.com/avatar/" + md5(api.user.email) + "?s=40&d=mm";
+    }
+  },
+  created() {
+    let statusbar = get_statusbar();
+    if (statusbar) {
+      this.statusbar_status = statusbar.status;
+      this.statusbar_body   = statusbar.body;
     }
   }
 }
---
+++ ui/components/statusbar.js
@@ -0,0 +1,32 @@
+import Cookies from "js-cookie";
+require("./statusbar.scss");
+
+export function get_statusbar() {
+  let statusbar = Cookies.getJSON("statusbar");
+  Cookies.remove("statusbar");
+  return statusbar;
+}
+
+export function set_statusbar(status, body) {
+  Cookies.set("statusbar", {
+    status: status,
+    body, body
+  });
+}
+
+export const StatusBar = {
+  name: "statusbar",
+  template: require("./statusbar.html"),
+  props: ["status", "body"],
+  data() {
+    return {
+      show: false
+    }
+  },
+  created() {
+    if (this.status) {
+      this.show = true;
+      setTimeout((()=>{ this.show = false; }).bind(this), 3000);
+    }
+  }
+}
