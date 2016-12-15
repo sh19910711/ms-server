e7d86bc6245935caae4ff572f74fc367d55de102
Seiya Nuta <nuta@seiya.me>
2016-12-15 13:45:21 +0900

    ui: introduce nprogress

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,22 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import NProgress from "nprogress";
 import App from "./app";
 import router from "./router";
 
 
 Vue.use(VueRouter);
+
+router.beforeEach((to, from, next) => {
+  NProgress.configure({
+    spinner: false
+  });
+  NProgress.start(0.4);
+  next();
+})
+
+router.afterEach((to, from) => {
+  NProgress.done();
+})
+
 new Vue(Vue.util.extend({ router }, App)).$mount("#app");
