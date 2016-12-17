997cbeb42201414f45c78346563a34413f2a2a12
Seiya Nuta <nuta@seiya.me>
2016-12-17 12:19:05 +0900

    make device name case-insensitive

--- app/models/device.rb
+++ app/models/device.rb
@@ -9,7 +9,8 @@ class Device < ApplicationRecord
   DEVICE_NAME_REGEX = /\A[a-zA-Z][a-zA-Z0-9\-\_]*\z/
   validates :user, presence: true
   validates :device_secret, presence: true
-  validates :name, uniqueness: { scope: :user }, presence: true,
+  validates :name, uniqueness: { scope: :user, case_sensitive: false },
+            presence: true,
             format: { with: DEVICE_NAME_REGEX }
   validates :board, inclusion: { in: SUPPORTED_BOARDS }
   validates :status, inclusion: { in: DEVICE_STATUSES }
